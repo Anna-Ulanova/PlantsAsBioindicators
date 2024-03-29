69c92e66e630d30a355231dce5ce0542e201f964
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2024-03-29 10:05:37 -0400

    Add files via upload

--- HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
+++ HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 from matplotlib import pyplot as plt
+import rasterio
+import numpy as np
 
 # AU Note: matplotlib as plt can allow you to give the images as an  output which is helpful 
 # helpful during debugging. Uncomment the plt.imshow() lines to see image outputs
@@ -16,7 +18,7 @@ def directories_of_hdr_files(script_path, directory_list, search_key):
         if search_key in files: 
             hdr_file_directory = os.path.join(root,search_key)
             directory_list.append(hdr_file_directory)
-        
+    print('directories_of_hdr_files complete')
     return directory_list
 ##-------------------------------------------------------------------------------------------###
 # Function takes in the directories of hdr files for SWIR and VNIR types, 
@@ -26,48 +28,51 @@ def matching_vnir_swir_files(swir_directories_with_hdr, vnir_directories_with_hd
     # Pairs HDR files if the last 2 folders match, EX: both are in GH_20230724/1_1_20230724_2023_07_24_08_16_25/raw_rd_rf.hdr
     # Creates a list pairs_for_overlap which can be overlapped/processed
     blank =[]
-    for directory in swir_directories_with_hdr: 
+    for directory in swir_directories_with_hdr:
+        # Splits lines based on the common delimiter: \ 
         sub_key = directory.split('\\')
+        # Joins the last three elements of the split, essentially limiting the search only for files within the last 2 folders
+        # This search finds only the hyperspectral SWIR/VNIR files if they were collected for the same plant, during the same time and date
         sub = '\\'.join(sub_key[-2:])
+        # Searches for directories that have a partial match for the common folders
         match_found_in_VNIR = [x for x in vnir_directories_with_hdr if sub in x]
-        row = [directory, match_found_in_VNIR[0]]
-        blank.append([row])
+        if directory != match_found_in_VNIR[0]:
+            row = [directory, match_found_in_VNIR[0]]
+            blank.append([row])
     # Turn on if you want to save the coupled list, useful for sanity checks. 
     with open("file.txt", "w") as output:
         output.write(str(blank))
-
+    print('matching_vnir_swir_files complete')
     return blank
-
+##-------------------------------------------------------------------------------------------###
+# This function reads the hyperspectral files using rasterio. Function also extracts the first 
+# band of the image for downstream processing. 
+def opening_hyperspectral_file(file_dir):
+    print(file_dir)
+    with rasterio.open(file_dir) as src: 
+        # Read the first band
+        band = src.read(1)  # Assuming bands are indexed starting from 1
+        plt.imshow(band)
+        plt.title('First Band')
+        plt.show()
+    print('opening_hyperspectral_file complete')
+    return band
 ##-------------------------------------------------------------------------------------------###
 # This function flips the SWIR image horizontally to make a "mirrored" image of itself
 # Inputs are the directory of the SWIR image, as well as the new folder that was created 
 # for saving the augmented SWIR images
 # Returns the pathway of the augmented SWIR image, as well as the "unique" name created for the image
 # (unique name is the combination of the two most recent folders which represent the sampling time and date)
-def horizontal_flipping(SWIR, flipped_image_dir):
+def horizontal_flipping(band, flipped_image_dir):
     # Changes directory to the folder where the flipped images are saved
     os.chdir(flipped_image_dir) 
-    hdr_image = cv2.imread(SWIR, cv2.IMREAD_UNCHANGED)
-    # Splits image directory based on the delimiter '\\'
-    SWIR_image_name = SWIR.split('\\')
-    # Joins the last 2 folder names with the actual file name to create a new tag for the image
-    flipped_image_name = '_'.join(SWIR_image_name[-3:])
-    # Check if the image is loaded successfully
-    if hdr_image is None:
-        print("Error: Unable to load the HDR image.")
-    else:
-        # Flip the image horizontally
-        flipped_image = cv2.flip(hdr_image, 1)
-        
-        plt.imshow(flipped_image)
-        # Save the flipped image
-        cv2.imwrite(unique_image_name, flipped_image)
-
-        # Creates directory for the image
-        print('image directory?:', os.path.join(flipped_image_dir,flipped_image_name))
-        flipped_SWIR_pathway = os.path.join(flipped_image_dir,flipped_image_name)
+    flipped_band = np.fliplr(band)
+    plt.imshow(flipped_band)
+    plt.title('Flipped SWIR')
+    plt.show()
     # Returns the flipped image directory for overlapping
-    return flipped_SWIR_pathway, flipped_image_name
+    print('horizontal_flipping complete')
+    return flipped_image_dir, flipped_band
     
 ##-------------------------------------------------------------------------------------------###
 # Function overlaps images based on edges found within the images. First the function detects edges
@@ -98,19 +103,20 @@ def overlapping_image(flipped_SWIR, VNIR, overlapped_images_dir, unique_image_na
     rows, cols, _ = im1.shape
     M = np.float32([[1, 0, dx], [0, 1, dy]])
     translated_im = cv2.warpAffine(im1, M, (cols, rows))
-    
+
     # Resize SWIR image to match VNIR image size
     resized_swir_image = cv2.resize(translated_im, (vnir_image.shape[1], vnir_image.shape[0]))
 
     # Save the resized SWIR image
-
     cv2.imwrite(unique_image_name, resized_swir_image)
+    print('overlapping_image complete')
+
 ##-------------------------------------------------------------------------------------------###
 
 ####################################### Input variables #######################################
 
 # Change target directory after testing 
-target_directory = "C:/Users/ulano/OneDrive/Desktop/Passion Projects"
+target_directory = "D:\AU"
 
 # Changes to directory to target path and reformats the delimiters to system's operating system
 os.chdir(target_directory)
@@ -119,9 +125,9 @@ script_path = os.getcwd()
 empty_swir_directories_with_hdr = []
 empty_vnir_directories_with_hdr = []
 
-swir_directories_with_hdr = directories_of_hdr_files(script_path, empty_swir_directories_with_hdr, search_key='raw_rd_rf.hdr')
+swir_directories_with_hdr = directories_of_hdr_files(script_path, empty_swir_directories_with_hdr, search_key='raw_rd_rf')
 
-vnir_directories_with_hdr = directories_of_hdr_files(script_path, empty_vnir_directories_with_hdr, search_key='raw_rd_rf.hdr')
+vnir_directories_with_hdr = directories_of_hdr_files(script_path, empty_vnir_directories_with_hdr, search_key='raw_rd_rf')
 
 # Creates a list of "coupled" SWIR and VNIR files based on their sampling time and date. If the sampling times are the same, 
 # directories of the files are extracted and entered into a 2D list (one column for SWIR directories, second column for VNIR directories)
@@ -132,20 +138,24 @@ pairs_for_overlap=pairs_for_overlap.pop(0)
 # Makes a new directory that saves the SWIR flipped image as backup
 flipped_image_dir = os.path.join(script_path, 'flipped_SWIR')
 # Creates a folder called "flipped_SWIR" off the main pathway 
-print(flipped_image_dir)
-os.mkdir(flipped_image_dir)
+#os.mkdir(flipped_image_dir)
 
 # Makes a new directory that saves the overlapped images as backup
 overlapped_images_dir = os.path.join(script_path,'overlapped_SWIR_VNIR')
-os.mkdir(overlapped_images_dir)
+#os.mkdir(overlapped_images_dir)
 for pair in pairs_for_overlap:
-    SWIR = pair[0][0]
-    VNIR = pair[0][1]
-    [flipped_SWIR, unique_image_name]= horizontal_flipping(SWIR,flipped_image_dir)
-    swir_image = cv2.imread(SWIR, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
-    vnir_image = cv2.imread(VNIR, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
-
+    print(pair)
+    my_list = pair
+    first_element_inner_list = my_list[0][0]
+    print("First element of the inner list:", first_element_inner_list)
+    SWIR = pair[1]
+    print('SWIR? \t', SWIR)
+    VNIR = pair[0]
+    print('VNIR? \t', VNIR)
+    swir_band = opening_hyperspectral_file(SWIR)
+    vnir_band = opening_hyperspectral_file(VNIR)
+    [flipped_SWIR, unique_image_name]= horizontal_flipping(swir_band,flipped_image_dir)
     # Align SWIR image with VNIR image based on object edges
-    overlapping_image(swir_image, vnir_image, unique_image_name)
+    overlapping_image(swir_band, vnir_band, overlapped_images_dir, unique_image_name)
 
 
