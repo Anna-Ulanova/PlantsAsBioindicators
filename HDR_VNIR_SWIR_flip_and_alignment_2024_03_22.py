7d718408527fa6d36d92b2d3d8c234fd585810d2
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2024-05-23 15:47:56 -0400

    Add files via upload

--- HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
+++ HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import numpy as np
 import pandas as pd
 import re
 #import cv2
@@ -41,40 +41,42 @@ def matching_vnir_swir_files(swir_directories_with_hdr, vnir_directories_with_hd
                 blank.append([directory, vnir])
     # Turn on if you want to save the coupled list, useful for sanity checks. 
     df = pd.DataFrame(blank, columns=['SWIR', 'VNIR'])
+    df.columns = ['SWIR', 'VNIR']
     df.to_excel('D:/reference.xlsx')
     return df
 ##-------------------------------------------------------------------------------------------###
 def time_offset(perfect_pairs, all_swir, all_vnir):
-    i = 0
-    print(perfect_pairs)
+    oof = 0
     improper = []
     time_offset_pairs = []
     for swir in all_swir: 
-        print(swir)
-        for i in range(len(perfect_pairs.index)):
-            print(perfect_pairs.iloc[i,0])
-            if  (swir not in perfect_pairs.iloc[i,0]):
-                formatted_swir = r'{}'.format(swir)
-                sp = re.split(r'[_]',formatted_swir)
-                no_second = '_'.join(sp[1:11])
-                print(sp)
-                print(no_second)
-                if no_second in all_vnir:
-                        time_offset_pairs.append([swir, no_second])
-                else:
-                    improper.append(swir)
-            else: 
-                print('oof')
-            i=i+1
+        # print(swir)
+        if  (swir not in perfect_pairs.loc[:,'SWIR']):
+            formatted_swir = r'{}'.format(swir)
+            sp = re.split(r'[_]',formatted_swir)
+            no_second = '_'.join(sp[3:9])
+           # print(sp)
+           # print(no_second)
+            if any(no_second in x for x in all_vnir):
+                    weird_bracket = [x for x in all_vnir if no_second in x]
+                    vnir = weird_bracket[0]
+                    # print(vnir)
+                    time_offset_pairs.append([swir, vnir])
+            else:
+                improper.append(swir)
+        else: 
+            oof = oof+1
+    print(oof)    
     return [time_offset_pairs, improper]
 
 ##-------------------------------------------------------------------------------------------###
 
 def unique_name(directory):
+    print(directory)
     parts = directory.split('\\')
     desired_substring = '_'.join(parts[2:4])
-
-    return desired_substring
+    overlapped_images_dir = target_dir+'\\'+desired_substring+'_stacked'
+    return overlapped_images_dir
 ####################################### Input variables #######################################
 
 # Change target directory after testing 
@@ -90,61 +92,20 @@ script_path = os.getcwd()
 #directories of the files are extracted and entered into a 2D list (one column for SWIR directories, second column for VNIR directories)
 
 pairs_for_overlap = matching_vnir_swir_files(swir_directories_with_hdr,vnir_directories_with_hdr)
-# Removes the first row from the list since it contains empty values
-#pairs_for_overlap=pairs_for_overlap.pop(0)
 
 [time_offset_pairs, improper] = time_offset(pairs_for_overlap, swir_directories_with_hdr, vnir_directories_with_hdr)
-print(time_offset_pairs)
-print('###########################')
+
 time_offset_pairs_df = pd.DataFrame(time_offset_pairs, columns=['SWIR','VNIR'])    
-time_offset_pairs_df = time_offset_pairs_df.drop_duplicates()
 overall_pairs = pd.concat([pairs_for_overlap,time_offset_pairs_df], ignore_index=True)
 os.chdir(target_directory)
 target_dir = os.getcwd()
-time_offset_pairs_df.to_excel('D:/time_offset_pairs.xlsx')
-overall_pairs.to_excel('D:/overall_pairs.xlsx')
-print(overall_pairs)
-print(overall_pairs.shape)
-# Wait for ENVI to open before proceeding
-# input("Press Enter to continue once ENVI has launched...")
-# Once ENVI is open, you can execute IDL commands or scripts within the ENVI/IDL environment
-# For example, executing an IDL script:
-#os.chdir(r'C:\Program Files\Harris\ENVI56\IDL88\help\online_help\Subsystems\idl')
-#idl_script_path = r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro"
+time_offset_pairs_df.to_csv('D:/time_offset_pairs.txt', sep='\t')
+overall_pairs.to_csv('D:/overall_pairs.txt', sep='\t')
 save_name = []
-# for pair in overall_pairs:
-#     for subpair in pair: 
-#         SWIR = subpair.iloc[:,1]
-#         VNIR = subpair[:,2]
-#         #print(SWIR, '\n\n\n\n')
-#         #print(VNIR, '\n\n\n\n')
-#         collection_date = unique_name(SWIR)
-#         # Makes a new directory that saves the overlapped images as backup
-#         overlapped_images_dir = target_dir+'\\'+collection_date+'\\stacked'
-#         print(overlapped_images_dir)
-#         save_name.append(overlapped_images_dir)
-#         #subprocess.call(["idl", "-e", r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro , '{}', '{}', '{}'".format(SWIR, VNIR, overlapped_images_dir)])
-#         #idl_command = ["idl", "-e", "@" + idl_script_path, SWIR, VNIR,overlapped_images_dir]
-#         # Execute IDL script within the ENVI/IDL environment
-#         #subprocess.call(idl_command)
-print(type(overall_pairs))
-print(overall_pairs.head())
-overall_pairs= overall_pairs.iloc[1:len(overall_pairs.shape)]
-for index, row in overall_pairs.iterrows():
-    print(pair)
-    SWIR = pair[0]
-    VNIR = pair[1]
-    #print(SWIR, '\n\n\n\n')
-    #print(VNIR, '\n\n\n\n')
-    collection_date = unique_name(SWIR, target_dir)
-    # Makes a new directory that saves the overlapped images as backup
-    overlapped_images_dir = target_dir+'\\'+collection_date+'\\stacked'
-    print(overlapped_images_dir)
-    save_name.append(overlapped_images_dir)
-    #subprocess.call(["idl", "-e", r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro , '{}', '{}', '{}'".format(SWIR, VNIR, overlapped_images_dir)])
-    #idl_command = ["idl", "-e", "@" + idl_script_path, SWIR, VNIR,overlapped_images_dir]
-    # Execute IDL script within the ENVI/IDL environment
-    #subprocess.call(idl_command)
-new = pairs_for_overlap.insert(save_name)
-new = pd.DataFrame(new)
-new.to_excel('D:/wow.xlsx')
+overall_save_names = overall_pairs['SWIR'].apply(unique_name)
+save_names = pd.DataFrame(np.array(overall_save_names), columns=['Save Names'])
+print(save_names.shape)
+save_names.to_excel('D:/save_name_nomenclature.xlsx')
+new = pd.concat([overall_pairs, save_names],axis=1, ignore_index=True)
+new = new.drop_duplicates(ignore_index=True)
+new.to_csv('D:/wow.csv', sep=',')
