95fe497c1605b1c7373325548131af2460ccc40f
Anna-Ulanova <89947745+Anna-Ulanova@users.noreply.github.com>
2024-05-23 10:08:11 -0400

    Add files via upload

--- HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
+++ HDR_VNIR_SWIR_flip_and_alignment_2024_03_22.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+import pandas as pd
+import re
 #import cv2
 #from matplotlib import pyplot as plt
 
@@ -10,14 +13,18 @@ import os
 # initiated list that will contain the directories of the HDR files, as well as a search key for
 # the files you want to isolate (EX:raw_rd_rf.hdr)
 # Returns a list of directories where HDR files are found
-def directories_of_hdr_files(script_path, directory_list, search_key):
+def directories_of_hdr_files(script_path, search_key):
+    swir_dir = []
+    vnir_dir = []
     for root, _ , files in os.walk(script_path, topdown=True):
     # Change search key 
         if search_key in files: 
             hdr_file_directory = os.path.join(root,search_key)
-            directory_list.append(hdr_file_directory)
-        
-    return directory_list
+            if 'SWIR' in hdr_file_directory: 
+                swir_dir.append(hdr_file_directory)
+            if 'VNIR' in hdr_file_directory: 
+                vnir_dir.append(hdr_file_directory)        
+    return [swir_dir, vnir_dir]
 ##-------------------------------------------------------------------------------------------###
 # Function takes in the directories of hdr files for SWIR and VNIR types, 
 # couples hdr directories if their sampling time matches (e.i. identical last 2 folders)
@@ -25,127 +32,119 @@ def directories_of_hdr_files(script_path, directory_list, search_key):
 def matching_vnir_swir_files(swir_directories_with_hdr, vnir_directories_with_hdr):
     # Pairs HDR files if the last 2 folders match, EX: both are in GH_20230724/1_1_20230724_2023_07_24_08_16_25/raw_rd_rf.hdr
     # Creates a list pairs_for_overlap which can be overlapped/processed
-    blank =[]
+    blank = []
     for directory in swir_directories_with_hdr: 
         sub_key = directory.split('\\')
-        sub = '\\'.join(sub_key[-2:])
-        match_found_in_VNIR = [x for x in vnir_directories_with_hdr if sub in x]
-        row = [directory, match_found_in_VNIR[0]]
-        blank.append([row])
+        sub = '\\'.join(sub_key[-3:])
+        for vnir in vnir_directories_with_hdr:
+            if sub in vnir: 
+                blank.append([directory, vnir])
     # Turn on if you want to save the coupled list, useful for sanity checks. 
-    with open("file.txt", "w") as output:
-        output.write(str(blank))
-
-    return blank
+    df = pd.DataFrame(blank, columns=['SWIR', 'VNIR'])
+    df.to_excel('D:/reference.xlsx')
+    return df
+##-------------------------------------------------------------------------------------------###
+def time_offset(perfect_pairs, all_swir, all_vnir):
+    i = 0
+    print(perfect_pairs)
+    improper = []
+    time_offset_pairs = []
+    for swir in all_swir: 
+        print(swir)
+        for i in range(len(perfect_pairs.index)):
+            print(perfect_pairs.iloc[i,0])
+            if  (swir not in perfect_pairs.iloc[i,0]):
+                formatted_swir = r'{}'.format(swir)
+                sp = re.split(r'[_]',formatted_swir)
+                no_second = '_'.join(sp[1:11])
+                print(sp)
+                print(no_second)
+                if no_second in all_vnir:
+                        time_offset_pairs.append([swir, no_second])
+                else:
+                    improper.append(swir)
+            else: 
+                print('oof')
+            i=i+1
+    return [time_offset_pairs, improper]
 
 ##-------------------------------------------------------------------------------------------###
-# This function flips the SWIR image horizontally to make a "mirrored" image of itself
-# Inputs are the directory of the SWIR image, as well as the new folder that was created 
-# for saving the augmented SWIR images
-# Returns the pathway of the augmented SWIR image, as well as the "unique" name created for the image
-# (unique name is the combination of the two most recent folders which represent the sampling time and date)
-def horizontal_flipping(SWIR, flipped_image_dir):
-    # Changes directory to the folder where the flipped images are saved
-    os.chdir(flipped_image_dir) 
-    hdr_image = cv2.imread(SWIR, cv2.IMREAD_UNCHANGED)
-    # Splits image directory based on the delimiter '\\'
-    SWIR_image_name = SWIR.split('\\')
-    # Joins the last 2 folder names with the actual file name to create a new tag for the image
-    flipped_image_name = '_'.join(SWIR_image_name[-3:])
-    # Check if the image is loaded successfully
-    if hdr_image is None:
-        print("Error: Unable to load the HDR image.")
-    else:
-        # Flip the image horizontally
-        flipped_image = cv2.flip(hdr_image, 1)
-        
-        plt.imshow(flipped_image)
-        # Save the flipped image
-        cv2.imwrite(unique_image_name, flipped_image)
 
-        # Creates directory for the image
-        print('image directory?:', os.path.join(flipped_image_dir,flipped_image_name))
-        flipped_SWIR_pathway = os.path.join(flipped_image_dir,flipped_image_name)
-    # Returns the flipped image directory for overlapping
-    return flipped_SWIR_pathway, flipped_image_name
-    
-##-------------------------------------------------------------------------------------------###
-# Function overlaps images based on edges found within the images. First the function detects edges
-# within the HDR file, and contours the edges. Then it finds bounding boxes for contours
-# Helpful links for Anna: https://datacarpentry.org/image-processing/edge-detection.html
-# Explanaition of Canny and edge detection: https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
-def overlapping_image(flipped_SWIR, VNIR, overlapped_images_dir, unique_image_name):
-    os.chdir(overlapped_images_dir)
-    # Function to align images based on edge features
+def unique_name(directory):
+    parts = directory.split('\\')
+    desired_substring = '_'.join(parts[2:4])
 
-    # Detect edges in the images
-    edges1 = cv2.Canny(flipped_SWIR, 100, 200)
-    edges2 = cv2.Canny(VNIR, 100, 200)
-
-    # Find contours in the edges
-    contours1, _ = cv2.findContours(edges1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours2, _ = cv2.findContours(edges2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Calculate bounding boxes for contours
-    x1, y1, w1, h1 = cv2.boundingRect(contours1[0])
-    x2, y2, w2, h2 = cv2.boundingRect(contours2[0])
-
-    # Calculate translation offsets
-    dx = x2 - x1
-    dy = y2 - y1
-
-    # Translate the input image
-    rows, cols, _ = im1.shape
-    M = np.float32([[1, 0, dx], [0, 1, dy]])
-    translated_im = cv2.warpAffine(im1, M, (cols, rows))
-    
-    # Resize SWIR image to match VNIR image size
-    resized_swir_image = cv2.resize(translated_im, (vnir_image.shape[1], vnir_image.shape[0]))
-
-    # Save the resized SWIR image
-
-    cv2.imwrite(unique_image_name, resized_swir_image)
-##-------------------------------------------------------------------------------------------###
-
+    return desired_substring
 ####################################### Input variables #######################################
 
 # Change target directory after testing 
-target_directory = "D:/"
-
+gen_directory = "D:/"
+target_directory = 'D:/overlapped_SWIR_VNIR'
 # Changes to directory to target path and reformats the delimiters to system's operating system
-os.chdir(target_directory)
+os.chdir(gen_directory)
 script_path = os.getcwd()
 
-empty_swir_directories_with_hdr = []
-empty_vnir_directories_with_hdr = []
+[swir_directories_with_hdr, vnir_directories_with_hdr] = directories_of_hdr_files(script_path, search_key='raw_rd_rf')
 
-swir_directories_with_hdr = directories_of_hdr_files(script_path, empty_swir_directories_with_hdr, search_key='raw_rd_rf.hdr')
+#Creates a list of "coupled" SWIR and VNIR files based on their sampling time and date. If the sampling times are the same, 
+#directories of the files are extracted and entered into a 2D list (one column for SWIR directories, second column for VNIR directories)
 
-vnir_directories_with_hdr = directories_of_hdr_files(script_path, empty_vnir_directories_with_hdr, search_key='raw_rd_rf.hdr')
-
-# Creates a list of "coupled" SWIR and VNIR files based on their sampling time and date. If the sampling times are the same, 
-# directories of the files are extracted and entered into a 2D list (one column for SWIR directories, second column for VNIR directories)
 pairs_for_overlap = matching_vnir_swir_files(swir_directories_with_hdr,vnir_directories_with_hdr)
 # Removes the first row from the list since it contains empty values
-pairs_for_overlap=pairs_for_overlap.pop(0)
+#pairs_for_overlap=pairs_for_overlap.pop(0)
 
-# Makes a new directory that saves the SWIR flipped image as backup
-flipped_image_dir = os.path.join(script_path, 'flipped_SWIR')
-# Creates a folder called "flipped_SWIR" off the main pathway 
-print(flipped_image_dir)
-os.mkdir(flipped_image_dir)
-
-# Makes a new directory that saves the overlapped images as backup
-overlapped_images_dir = os.path.join(script_path,'overlapped_SWIR_VNIR')
-os.mkdir(overlapped_images_dir)
-for pair in pairs_for_overlap:
-    SWIR = pair[0][0]
-    VNIR = pair[0][1]
-    [flipped_SWIR, unique_image_name]= horizontal_flipping(SWIR,flipped_image_dir)
-    swir_image = cv2.imread(SWIR, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
-    vnir_image = cv2.imread(VNIR, cv2.IMREAD_ANYDEPTH | cv2.IMREAD_COLOR)
-
-    # Align SWIR image with VNIR image based on object edges
-    overlapping_image(swir_image, vnir_image, unique_image_name)
-
-
+[time_offset_pairs, improper] = time_offset(pairs_for_overlap, swir_directories_with_hdr, vnir_directories_with_hdr)
+print(time_offset_pairs)
+print('###########################')
+time_offset_pairs_df = pd.DataFrame(time_offset_pairs, columns=['SWIR','VNIR'])    
+time_offset_pairs_df = time_offset_pairs_df.drop_duplicates()
+overall_pairs = pd.concat([pairs_for_overlap,time_offset_pairs_df], ignore_index=True)
+os.chdir(target_directory)
+target_dir = os.getcwd()
+time_offset_pairs_df.to_excel('D:/time_offset_pairs.xlsx')
+overall_pairs.to_excel('D:/overall_pairs.xlsx')
+print(overall_pairs)
+print(overall_pairs.shape)
+# Wait for ENVI to open before proceeding
+# input("Press Enter to continue once ENVI has launched...")
+# Once ENVI is open, you can execute IDL commands or scripts within the ENVI/IDL environment
+# For example, executing an IDL script:
+#os.chdir(r'C:\Program Files\Harris\ENVI56\IDL88\help\online_help\Subsystems\idl')
+#idl_script_path = r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro"
+save_name = []
+# for pair in overall_pairs:
+#     for subpair in pair: 
+#         SWIR = subpair.iloc[:,1]
+#         VNIR = subpair[:,2]
+#         #print(SWIR, '\n\n\n\n')
+#         #print(VNIR, '\n\n\n\n')
+#         collection_date = unique_name(SWIR)
+#         # Makes a new directory that saves the overlapped images as backup
+#         overlapped_images_dir = target_dir+'\\'+collection_date+'\\stacked'
+#         print(overlapped_images_dir)
+#         save_name.append(overlapped_images_dir)
+#         #subprocess.call(["idl", "-e", r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro , '{}', '{}', '{}'".format(SWIR, VNIR, overlapped_images_dir)])
+#         #idl_command = ["idl", "-e", "@" + idl_script_path, SWIR, VNIR,overlapped_images_dir]
+#         # Execute IDL script within the ENVI/IDL environment
+#         #subprocess.call(idl_command)
+print(type(overall_pairs))
+print(overall_pairs.head())
+overall_pairs= overall_pairs.iloc[1:len(overall_pairs.shape)]
+for index, row in overall_pairs.iterrows():
+    print(pair)
+    SWIR = pair[0]
+    VNIR = pair[1]
+    #print(SWIR, '\n\n\n\n')
+    #print(VNIR, '\n\n\n\n')
+    collection_date = unique_name(SWIR, target_dir)
+    # Makes a new directory that saves the overlapped images as backup
+    overlapped_images_dir = target_dir+'\\'+collection_date+'\\stacked'
+    print(overlapped_images_dir)
+    save_name.append(overlapped_images_dir)
+    #subprocess.call(["idl", "-e", r"C:\Users\RDCRLAAU\Desktop\Plant as bioindicators\IDL Image Processing\main_operation.pro , '{}', '{}', '{}'".format(SWIR, VNIR, overlapped_images_dir)])
+    #idl_command = ["idl", "-e", "@" + idl_script_path, SWIR, VNIR,overlapped_images_dir]
+    # Execute IDL script within the ENVI/IDL environment
+    #subprocess.call(idl_command)
+new = pairs_for_overlap.insert(save_name)
+new = pd.DataFrame(new)
+new.to_excel('D:/wow.xlsx')
